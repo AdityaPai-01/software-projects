cb18edab30d69a9e5feef09edd03e6ef739ff7bd
Aditya Pai <aditya.a.pai.tech@gmail.com>
2026-09-19 19:35:15 +0530

    made some changes in the code, planning to give the project an overhaul

--- user_login/helpers.py
+++ user_login/helpers.py
@@ -12,13 +12,15 @@ if getattr(sys, 'frozen', False): #Running script as .exe
     BaseDirectory = Path(sys.executable).resolve().parent
 else: #Running script as a script
     BaseDirectory = Path(__file__).resolve().parent
+
 DataDir = BaseDirectory / "data"
 DataDir.mkdir(parents=True, exist_ok=True)
 User_Manager = UserManager()
 JSON_Storage = JSONStorage(DataDir)
-User_Manager.importData(JSON_Storage.load_data())
 Log_Manager = LogManager(DataDir)
+User_Manager.importData(JSON_Storage.load_data())
 
+# Clearing the terminal for cleaner UI.
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
     return "1: clear terminal executed"
