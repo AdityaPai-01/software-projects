06e1109a219aede388f5709316762132afc9b6c6
Aditya Pai <aditya.a.pai.tech@gmail.com>
2026-09-25 15:05:15 +0530

    added exception handling to logging configuration

--- user_login/logging_config.py
+++ user_login/logging_config.py
@@ -5,7 +5,10 @@ logfile = Path("data/logdata.log")
 
 # setting up log
 def setup_log():
-    logging.basicConfig(level=logging.INFO, 
+    try:
+        logging.basicConfig(level=logging.INFO, 
                         format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
                         handlers=[logging.FileHandler(logfile, encoding='utf-8')])
+    except Exception as e:
+        return f"Error: {e}"
     
